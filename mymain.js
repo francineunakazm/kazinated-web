560a23e60d90b3c2c56e1a363a26f25777fcfdac
francineunakazm <74522592+francineunakazm@users.noreply.github.com>
2024-03-07 23:24:02 +0800

    Delete mymain.js

--- mymain.js
+++
@@ -1,7 +0,0 @@
-var typed= new Typed(".text", {
-    strings:["an Artist" , "a Musician" , "an aspiring Software Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
